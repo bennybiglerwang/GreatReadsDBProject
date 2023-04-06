ff0989daff70d43127183da676ba656e63892254
mrb5ay <mrb5ay@virginia.edu>
2023-04-06 17:40:36 -0400

    book detail page a little more fleshed out

--- book_page.php
+++ book_page.php
@@ -24,6 +24,8 @@
 		
 		<link href="./index_files/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		
 	</head>
 	
 	<style>
@@ -60,6 +62,33 @@
 		?>
 		</p>
 		
+		<div class="container">
+		  <h1> Join the discussion! </h1>  
+
+		  <form name="review_form" action="book_page.php" method="post">   
+		  <div class="row mb-3 mx-3">
+			<p> Review <i><?php echo " ".$_POST['title']; ?></i>: </p>
+			<textarea rows="6" cols="70" name="r_text" maxlength="1000" required></textarea>      
+		  </div>  
+		  <div class="row" style="position:relative;padding-bottom:15px;" >
+			<input type="radio" class="form-control" name="star_rating" value="1" style="position:absolute; left:20px; z-index:950; opacity:0 " required />
+			<input type="radio" class="form-control" name="star_rating" value="2" style="position:absolute; left:40px; z-index:950; opacity:0" required />
+			<input type="radio" class="form-control" name="star_rating" value="3" style="position:absolute; left:60px; z-index:950; opacity:0" required />
+			<input type="radio" class="form-control" name="star_rating" value="4" style="position:absolute; left:80px; z-index:950; opacity:0" required />
+			<input type="radio" class="form-control" name="star_rating" value="5" style="position:absolute; left:100px; z-index:950; opacity:0" required />
+			<input type="hidden" name="ISBN" value=<?php echo $_POST['ISBN']; ?>>
+			<input type="hidden" name="title" value=<?php echo $_POST['ISBN']; ?>>
+			<span class="fa fa-star" style="position:absolute;left:20px"></span>
+			<span class="fa fa-star" style="position:absolute;left:40px"></span>
+			<span class="fa fa-star" style="position:absolute;left:60px"></span>
+			<span class="fa fa-star" style="position:absolute;left:80px"></span>
+			<span class="fa fa-star" style="position:absolute;left:100px"></span>
+		  </div>
+		  <div style="position:relative">
+		  <input type="submit" value="Review">
+		  </div>
+		</form>
+		
 		<h1>Reviews: </h1>
 		
 		<?php 
@@ -89,6 +118,12 @@
 			return $results;
 		}
 		
+		function post_review($r_text){
+		}
+		
+		function post_comment($c_text){
+		}
+		
 		if(isset($_POST['ISBN'])){
 			$reviews = select_reviews_for_book($_POST['ISBN']);
 		} else {
@@ -96,7 +131,7 @@
 		}
 		?>
 		<?php foreach ($reviews as $review): ?>
-			<div class="solid">
+			<div class="solid" style="background-color:gainsboro">
 			<h6><?php echo $review['star_rating']." out of 5 stars. Reviewed on ".$review['r_date']."." ?></h6>
 			<h3><?php echo $review['username'].": " ?></h3>
 			<p> <?php echo $review['r_text'] ?></p>
@@ -105,11 +140,17 @@
 			$comments = select_comments_for_review($review['review_num']);
 			foreach ($comments as $comment): 
 			?>
-			<div class = "dotted">
-			<h3 class = "sm"><?php echo $comment['username']." replied on ".$comment['c_date'].": " ?></h3>
-			<p class ="sm"> <?php echo $comment['c_text'] ?></p>
+			<div class="solid" style = "position:relative; padding-bottom:60px; background-color:silver">
+				<h5 class = "sm" style="position:absolute; left:20px; top:0px"><?php echo $comment['username']." replied on ".$comment['c_date'].": " ?></h3>
+				<p class ="sm" style="position:absolute; left:20px; top: 20px"> <?php echo "> ".$comment['c_text'] ?></p>
 			</div>
 			<?php endforeach ?>
+			<div class="solid" style = "position:relative; padding-bottom:60px; background-color:silver">
+				<form name="comment_form" action="book_page.php" method="post">
+				<input type="text" class="form-control" name="c_text" style="position:absolute; left:20px; top:0px" required>
+				<input type="submit" class="form-control" value="Reply" style="position:absolute; left:20px; top:28px">
+			</form>
+			</div>
 		<?php endforeach ?>
 	</body>
 	

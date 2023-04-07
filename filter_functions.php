bc92527af6fab72a88502781f9220cfe1805de23
hk4tnh <hk4tnh@virginia.edu>
2023-04-06 21:46:04 -0400

    search filters selection/functions
    addedd a new helper php file: filter_functions to contain all the different filter queries

---
+++ filter_functions.php
@@ -0,0 +1,73 @@
+<?php
+function selectAllBooks(){
+
+    global $db;
+    
+    $query="select * from books";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+    return $result;
+}
+
+// $filter_options = array(
+//     'title' => 'Title',
+//     'author' => 'author',
+//     'isbn' => 'isbn',
+//     'language' => 'language'
+
+// )
+    
+function filterByTitle(){
+
+    global $db;
+
+    $query = "SELECT * FROM books
+            WHERE (`title` LIKE '%".$_POST['search_query']."%')";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        return $results;
+}
+
+function filterByAuthor(){
+
+    global $db;
+
+    $query = "SELECT * FROM books
+            WHERE (`authors` LIKE '%".$_POST['search_query']."%')";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        return $results;
+}
+
+function filterByISBN(){
+
+    global $db;
+
+    $query = "SELECT * FROM books
+            WHERE (`ISBN` LIKE '%".$_POST['search_query']."%')";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        return $results;
+}
+
+function filterByLanguage(){
+
+    global $db;
+
+    $query = "SELECT * FROM books
+            WHERE (`language_code` LIKE '%".$_POST['search_query']."%')";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        return $results;
+}
+?>
--- search_page.php
+++ search_page.php
@@ -1,5 +1,8 @@
-<?php session_start();
-require 'connect-db.php';
+<?php session_start(); ?>
+<?php require 'connect-db.php'; ?>
+<?php require 'filter_functions.php'; ?>
+
+<?php
 if(isset($_POST['username'])){
    if(check_user_exists($_POST['username'])){
       $_SESSION['username'] = $_POST['username'];
@@ -25,46 +28,43 @@ function check_user_exists($username){
    }
    return false;
 }
+
  
-function selectAllBooks(){
+$search_results = selectAllBooks();
+//$search_results = filterByTitle();
 
-    global $db;
-    
-    $query="select * from books";
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $result = $statement->fetchAll();
-    $statement->closeCursor();
-    return $result;
+if (isset($_POST['filter_options'])) {
+
+    $filter = $_POST['filter_options'];
+    echo $filter;
+    if ($filter == 'title'){
+        $search_results = filterByTitle();
+    }   
+    if ($filter == 'authors'){
+        $search_results = filterByAuthor();
+    }
+    if ($filter == 'isbn'){
+        $search_results = filterByISBN();
+    }
+    if ($filter == 'language'){
+        $search_results = filterByLanguage();
+    }
 }
-    
-function filterByTitle(){
-
-    global $db;
-
-    $query = "SELECT * FROM books
-            WHERE (`title` LIKE '%".$_GET['search_query']."%')";
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $results = $statement->fetchAll();
-        $statement->closeCursor();
-        return $results;
-}
- 
-if (isset($_POST['option_selected']))
-{
-    $filter = $_POST['option_selected'];
-    echo "$filter";
+else {
+    echo "no set";
 }
 
-if ($filter == 'title'){
-    $search_results = filterByTitle();
-}
-
-
-//var_dump($search_results);
-
+//<?php if(isset($_POST["filter_options"]) && $_POST["filter_options"]  == "Author") echo "selected";
 ?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 
@@ -75,45 +75,38 @@ if ($filter == 'title'){
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   
 <div class="container">
-        <div class="row">
-            <div class="col-lg-12 card-margin">
-                <div class="card search-form">
-                    <div class="card-body p-0">
-                        <form id="search-form" method="GET">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="row no-gutters">
-                                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
-                                          <form method="post" action="search_page.php">  
-                                            <select name="option_selected">
-                                              <option name='title'>Title</option>
-                                              <option name='author'>Author</option>
-                                              <option name='isbn'>ISBN</option>
-                                              <option name='language'>Language</option>
-                                            </select>
-                                          </form> 
-                                        </div>
-                                        <div class="col-lg-8 col-md-6 col-sm-12 p-0">
-                                            <input type="text" placeholder="Search..." class="form-control"name="search_query">
-                                        </div>
-                                        <div class="col-lg-1 col-md-3 col-sm-12 p-0">
-                                            <button type="submit" class="btn btn-base">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-                                            </button>
-                                        </div>
+    <div class="row">
+        <div class="col-lg-12 card-margin">
+            <div class="card search-form">
+                <div class="card-body p-0">
+                    <form id="search-form" action="search_page.php" method="POST">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="row no-gutters">
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                        <select  class="form-control" name="filter_options">
+                                            <option value='title'>Title</option>
+                                            <option value='authors'>Author(s)</option>
+                                            <option value='isbn'>ISBN</option>
+                                            <option value='language'>Language Code</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-8 col-md-6 col-sm-12 p-0">
+                                        <input type="text" placeholder="Search..." class="form-control"name="search_query">
+                                    </div>
+                                    <div class="col-lg-1 col-md-3 col-sm-12 p-0">
+                                        <button type="submit" class="btn btn-base">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    </div>
 </div>
 </html>
 
@@ -133,7 +126,7 @@ if ($filter == 'title'){
          <thead>
          <tr style="background-color:#B0B0B0">
             <th>Title</th>      
-            <th>Author</th>         
+            <th>Author(s)</th>         
             <th>Average Rating</th> 
             <th>Details?</th>        
             <th>Rate?</th>  
@@ -142,7 +135,7 @@ if ($filter == 'title'){
          <?php foreach ($search_results as $item): ?>
             <tr>
                <td><?php echo $item['title']; ?></td>
-               <td><?php echo $item['author']; ?></td>        
+               <td><?php echo $item['authors']; ?></td>        
                <td><?php echo $item['average_rating']; ?></td>
                <td>
                   <form action="search_page.php" method="post">

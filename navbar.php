df81123992ee9b99230af1438a1ffb4672db9152
bennybiglerwang <98785166+bennybiglerwang@users.noreply.github.com>
2023-04-07 15:41:01 -0400

    Changed navbar
    Made it pretty. We can discuss color choice but I like

--- navbar.php
+++ navbar.php
@@ -1,10 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<style>
+
+.topnav {
+  background-color: #FECD5A;
+  overflow: hidden;
+}
+
+.topnav a {
+  float: right;
+  color: #808080;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #FFA500;
+  color: white;
+}
+</style>
+
 <header class="navbar navbar-default navbar-static-top">
     <div class="container-fluid">
         <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <a class="active" href="search_page.php">Home</a>                  
+            <ul class="topnav">
+                <a class="active" href="sign_in.php">Log In</a>
+                 <a href="search_page.php">Home</a> 
                 <a href="profile">Profile</a>
             </ul>
         </div>
     </div>
 </header>
+
+
+</html>

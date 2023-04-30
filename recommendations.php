471e001be9d15b73b9c22216802bca84b0966eda
hk4tnh <hk4tnh@virginia.edu>
2023-04-30 01:39:21 -0400

    create recommendations

--- recommendations.php
+++ recommendations.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require 'connect-db.php';
+include('navbar.php');
 
 if(isset($_SESSION['username'])){
     $username = $_SESSION['username'];
@@ -11,15 +12,26 @@ if(isset($_SESSION['username'])){
 
 } else echo 'You must be logged in to view your recommendations.';
 
-var_dump($recs);
+//var_dump($recs);
 
 function get_user_recs($username){
     global $db;
-    $query = "SELECT * FROM recommends WHERE username1 = :username;";
+    $query = "SELECT * FROM recommends WHERE username2 = :username;";
     $statement = $db->prepare($query);
     $statement->bindValue(':username', $username);
     $statement->execute();
-    $results = $statement->fetch();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    return $results;
+}
+
+function get_book($book_isbn){
+    global $db;
+    $query = "SELECT title FROM books WHERE isbn = :book_isbn;";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':book_isbn', $book_isbn);
+    $statement->execute();
+    $results = $statement->fetchAll();
     $statement->closeCursor();
     return $results;
 }
@@ -30,15 +42,35 @@ function get_user_recs($username){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-</head>
-<body>
+</head> 
+
+<html>  
 <div class="container">
-    <strong>Your Recommendations</strong>
-    <?php foreach ($recs as $item): ?>
-            <tr>
-               <td><?php echo $item['book_isbn']; ?></td>
-               <td><?php echo $item[2]; ?></td>        
-            </tr>
-         <?php endforeach; ?>
-</body>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<link href="search-filter.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+    <div class="row justify-content-center">  
+      <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+         <thead>
+         <tr style="background-color:#B0B0B0">
+            <th>Book</th>      
+            <th>From</th>    
+            <th></th>       
+         </tr>
+         </thead>
+         <strong>Your Recommendations</strong>
+            <?php foreach ($recs as $item): ?>
+                <tr>
+                <?php $book_title = get_book($item['book_isbn']); ?>
+                <!--<?php var_dump($book_title); ?>-->
+                <td><?php echo $book_title[0]['title']; ?></td>
+                <td><?php echo $item['username1']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+         </table>
+    </div>  
+
+</div>
 </html>
